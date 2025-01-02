471fb60cbad7844385ec6468768e2830bfdebb20
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-02 23:24:00 +0530

    Newletter box section added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../Components/Hero'
 import LatestCollection from '../Components/LatestCollection'
 import BestSeller from '../Components/bestSeller'
 import Policy from '../Components/Policy'
+import NewletterBox from '../Components/NewletterBox'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <Policy/>
+      <NewletterBox/>
     </div>
   )
 }
