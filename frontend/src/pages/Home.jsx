7ef7c6afc63ba9de7f0af4d31107e3f34ffb049d
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-02 16:14:35 +0530

    Hero component is completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Hero from '../Components/Hero'
 
 const Home = () => {
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
